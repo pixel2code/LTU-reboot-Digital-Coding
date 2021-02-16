ea6ea43431b4d439ca01d565b7de02669f557ea9
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-02-16 19:40:17 +0000

    Update task-3.2.2.js
    create: fullname function, took in two parameters and concat both parameters together

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,9 @@
+function fullName( firstName, lastName ) {
+
+  console.log( firstName + ' ' + lastName );
+
+}
+
+fullName('Luke', 'Ashton-Johnson');
+fullName('Peter', 'Jones');
+fullName('Neil', 'Armstrong');
