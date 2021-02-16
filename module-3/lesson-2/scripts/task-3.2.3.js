4fba1adffd97a1e59bf0d7cf6c14b440b36a0066
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-02-16 20:48:37 +0000

    Update task-3.2.3.js
    Add: function fullName

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -1,14 +1,30 @@
 /* EXAMPLE */
 
-function greet(greeting, name) {
-  return greeting + ' ' + name;
-}
+// function greet(greeting, name) {
+//   return greeting + ' ' + name;
+// }
 
-var message = greet('Hello', 'Luke');
-    console.log( message ); // Output: Hello Luke
+// var message = greet('Hello', 'Luke');
+//     console.log( message ); // Output: Hello Luke
 
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+
+// function fullName( firstName, lastName ) {
+
+//    return firstName + ' ' + lastName ;
+
+// }
+
+// var person1 = fullName('Luke', 'Ashton-Johnson');
+
+
+// console.log( fullName('Luke', 'Ashton-Johnson') );
+// console.log( fullName('Peter', 'James') );
+// console.log( fullName('Aimee', 'Anderson') );
+
+
+// console.log( 'person1 variable: ' + person1 );
 
 // ADD YOUR CODE ABOVE HERE
