c5b73f9d869fcf22f0b63b18e722ae5def0621d1
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-02-16 20:49:40 +0000

    Update task-3.2.5.js
    Add: temperature program with conditionals

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,19 @@
+var temperature = 49;
+
+if ( temperature < 0 ) {
+
+  console.log( 'Stay inside');
+
+} else if ( temperature < 30 ) {
+
+  console.log( 'Wear a coat and a hat');
+
+} else if ( temperature < 50 ) {
+
+  console.log( 'Wear a coat');
+
+} else {
+
+  console.log( 'just pants and vest is fine');
+
+}
