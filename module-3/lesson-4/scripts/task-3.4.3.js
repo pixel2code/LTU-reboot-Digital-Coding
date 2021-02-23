8a795edfd2ac06fa5040f418d50b5036da748daa
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-02-23 20:12:43 +0000

    Create task-3.4.3.js
    add: favourite foods array and loop over it

---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,15 @@
+let favouriteFoods = [ 
+  'apples', 
+  'oranges' , 
+  'pears' , 
+  'ice cream' , 
+  'hamburgers',
+  'kebab',
+  'chocolate'
+];
+
+for ( let index = 0; index < favouriteFoods.length; index++ ) {
+
+  console.log( `${ index } : ${ favouriteFoods[ index ] }`);
+
+}
