0f8db09e112ea352911487fdc30eb50264b02c62
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-02-23 19:35:45 +0000

    Create task-3.4.1.js
    create: for loop for seven times table

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,21 @@
+/*
+
+for ( begin; condition; step ) {
+
+  Loop body
+
+}
+
+*/
+
+for ( var index = 1; index <= 12; index++ ) {
+
+  // console.log( index );
+
+  var multiplyBy = 7;
+
+  var result = index * multiplyBy;
+
+  console.log( `${ index } * ${ multiplyBy } = ${ result }`  );
+
+}
