59bdff8e9cacdbd2e535d18d01550c6d84f9baed
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-02-25 18:53:42 +0000

    Create task-3.4.5.js
    add: a method to our favouriteRecipe object called letsCook

---
+++ module-3/lesson-4/scripts/task-3.4.5.js
@@ -0,0 +1,23 @@
+const favouriteRecipe = {
+  recipeTitle : 'Spaghetti Bolognese',
+  servings: 6,
+  ingredients: [
+    '1 tbsp olive oil', 
+    '500g mince beef', 
+    'tomato sauce'
+  ],
+  directions: [
+    'Make Bolognese sauce',
+    'step 2',
+    'step 3'
+  ],
+  letsCook: function() {
+    console.log( "I'm hungry!, let's cook " + favouriteRecipe.recipeTitle );
+  },
+  makeRecipe() {
+    console.log( "To cook " + favouriteRecipe.recipeTitle + " you will needed the following: " + favouriteRecipe.ingredients);
+  },
+}
+
+favouriteRecipe.letsCook();
+favouriteRecipe.makeRecipe();
